791b9ce63a2807662e64d5e1846d7543ff531e15
Julio Cesar de Azeredo <jcdazeredo@gmail.com>
2024-02-17 13:14:58 +0100

    Add UpdatePitchRequest model

--- pitch_health_monitor/models/bodies.py
+++ pitch_health_monitor/models/bodies.py
@@ -13,3 +13,12 @@ class CreatePitchRequest(BaseModel):
     next_scheduled_maintenance: datetime = Field(..., description="Date and time of the next scheduled maintenance")
     current_condition: int = Field(1, ge=1, le=10, description="Current condition rating of the pitch (from 1 to 10)")
     replacement_date: datetime = Field(..., description="Date and time for scheduled replacement of the pitch")
+
+class UpdatePitchRequest(BaseModel):
+    name: str = Field(None, description="Name of the pitch")
+    location: Location = Field(None, description="Location of the pitch (city and country)")
+    turf_type: TurfType = Field(None, description="Type of turf (e.g., Natural, Artificial, Hybrid)")
+    last_maintenance_date: datetime = Field(None, description="Date and time of the last maintenance")
+    next_scheduled_maintenance: datetime = Field(None, description="Date and time of the next scheduled maintenance")
+    current_condition: int = Field(None, ge=1, le=10, description="Current condition rating of the pitch (from 1 to 10)")
+    replacement_date: datetime = Field(None, description="Date and time for scheduled replacement of the pitch")
