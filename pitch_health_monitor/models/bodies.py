1bb7e2e6a64816556c709350627d31ecfcefed98
Julio Cesar de Azeredo <jcdazeredo@gmail.com>
2024-02-17 21:46:04 +0100

    Update models to new types

--- pitch_health_monitor/models/bodies.py
+++ pitch_health_monitor/models/bodies.py
@@ -6,19 +6,13 @@ from pydantic import BaseModel, Field
 from pitch_health_monitor.models.schemas import Location, TurfType
 
 class CreatePitchRequest(BaseModel):
-    name: str = Field(..., description="Name of the pitch")
-    location: Location = Field(..., description="Location of the pitch (city and country)")
-    turf_type: TurfType = Field(..., description="Type of turf (e.g., Natural, Artificial, Hybrid)")
-    last_maintenance_date: datetime = Field(..., description="Date and time of the last maintenance")
-    next_scheduled_maintenance: datetime = Field(..., description="Date and time of the next scheduled maintenance")
-    current_condition: int = Field(1, ge=1, le=10, description="Current condition rating of the pitch (from 1 to 10)")
-    replacement_date: datetime = Field(..., description="Date and time for scheduled replacement of the pitch")
+    name: str = Field(..., example="Fritz-Walter-Stadion", description="Name of the pitch")
+    location: Location = Field(..., example=Location(city="Kaiserslautern", country="Germany", latitude=49.43432260240235, longitude=7.7766827641282985), description="Location of the pitch (city and country)")
+    turf_type: TurfType = Field(..., example=TurfType.natural, description="Type of turf (e.g., Natural, Artificial, Hybrid)")
+    current_condition: int = Field(10, ge=1, le=10, example=10, description="Current condition rating of the pitch (from 1 to 10)")
 
 class UpdatePitchRequest(BaseModel):
-    name: str = Field(None, description="Name of the pitch")
-    location: Location = Field(None, description="Location of the pitch (city and country)")
-    turf_type: TurfType = Field(None, description="Type of turf (e.g., Natural, Artificial, Hybrid)")
-    last_maintenance_date: datetime = Field(None, description="Date and time of the last maintenance")
-    next_scheduled_maintenance: datetime = Field(None, description="Date and time of the next scheduled maintenance")
-    current_condition: int = Field(None, ge=1, le=10, description="Current condition rating of the pitch (from 1 to 10)")
-    replacement_date: datetime = Field(None, description="Date and time for scheduled replacement of the pitch")
+    name: str = Field(None, example="Fritz-Walter-Stadion", description="Name of the pitch")
+    location: Location = Field(None, example=Location(city="Kaiserslautern", country="Germany", latitude=49.43432260240235, longitude=7.7766827641282985), description="Location of the pitch (city and country)")
+    turf_type: TurfType = Field(None, example=TurfType.natural, description="Type of turf (e.g., Natural, Artificial, Hybrid)")
+    current_condition: int = Field(None, ge=1, le=10, example=10, description="Current condition rating of the pitch (from 1 to 10)")
--- pitch_health_monitor/models/responses.py
+++ pitch_health_monitor/models/responses.py
@@ -1,5 +1,6 @@
 
 from datetime import datetime
+from typing import Optional
 from uuid import UUID
 
 from pydantic import BaseModel, Field
@@ -9,10 +10,14 @@ from pitch_health_monitor.models.schemas import Location, TurfType
 
 class PitchResponse(BaseModel):
     uuid: UUID = Field(..., description="UUID of the pitch")
-    name: str = Field(..., description="Name of the pitch")
-    location: Location = Field(..., description="Location of the pitch (city and country)")
-    turf_type: TurfType = Field(..., description="Type of turf (e.g., Natural, Artificial, Hybrid)")
-    last_maintenance_date: datetime = Field(..., description="Date and time of the last maintenance")
-    next_scheduled_maintenance: datetime = Field(..., description="Date and time of the next scheduled maintenance")
-    current_condition: int = Field(1, ge=1, le=10, description="Current condition rating of the pitch (from 1 to 10)")
-    replacement_date: datetime = Field(..., description="Date and time for scheduled replacement of the pitch")
+    created_at: datetime = Field(default_factory=datetime.utcnow, description="Date and time when the pitch was created")  
+    last_checked_at: Optional[datetime] = Field(None, description="Date and time when the pitch condition was last checked")   
+
+    name: str = Field(..., example="Fritz-Walter-Stadion", description="Name of the pitch")
+    location: Location = Field(..., example=Location(city="Kaiserslautern", country="Germany", latitude=49.43432260240235, longitude=7.7766827641282985), description="Location of the pitch")
+    turf_type: TurfType = Field(..., example=TurfType.natural, description="Type of turf")
+    current_condition: int = Field(..., ge=1, le=10, example=10, description="Current condition rating of the pitch (from 1 to 10)")
+
+    last_maintenance_date: Optional[datetime] = Field(None, description="Date and time of the last maintenance")
+    next_scheduled_maintenance: Optional[datetime] = Field(None, description="Date and time of the next scheduled maintenance")
+    replacement_date: Optional[datetime] = Field(None, description="Date and time for scheduled replacement of the pitch")
