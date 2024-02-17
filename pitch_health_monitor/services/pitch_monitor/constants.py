2acda0e1c7ecc27bdbd546c92980abe386f4c242
Julio Cesar de Azeredo <jcdazeredo@gmail.com>
2024-02-17 21:48:25 +0100

    Create base for pitch heath monitor service

---
+++ pitch_health_monitor/services/pitch_monitor/constants.py
@@ -0,0 +1,13 @@
+from pitch_health_monitor.models.schemas import TurfType
+
+DRYING_TIME = {
+    TurfType.artificial: 12,
+    TurfType.hybrid: 24,
+    TurfType.natural: 36
+}
+
+RAIN_TOLERANCE = {
+    TurfType.artificial: 6,
+    TurfType.hybrid: 4,
+    TurfType.natural: 3
+}
